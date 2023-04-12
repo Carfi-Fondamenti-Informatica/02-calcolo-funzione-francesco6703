a84f661a3499f6c470aca0fdeefbe6a612342cf6
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-04-12 15:56:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+	float a, b, x, y;
+	cin >> a >> b >> x >> y;
+	if(x<0 && y>0)
+	{
+		cout << a*x-b*y;
+	}else{
+		if(x>=0 && y<=0)
+		{
+			cout << a*x*x - b*y;
+		}else{
+			cout << a*x + b*y*y;
+		}
+	}
    return 0;
 }
